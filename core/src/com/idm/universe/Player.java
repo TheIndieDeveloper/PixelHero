ca5634d9129b87da8d2b729aa77eac8f0bad5fbd
IndieDeveloper <tomas0610@hotmail.com>
2016-09-20 18:07:33 +0200

    Player Input
    added player input

--- core/src/com/idm/universe/Player.java
+++ core/src/com/idm/universe/Player.java
@@ -1,6 +1,8 @@
 package com.idm.universe;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
@@ -8,6 +10,8 @@ import com.badlogic.gdx.math.Vector3;
 public class Player {
 	
 	private OrthographicCamera camera;
+	private float speed = 2.2f;
+	
 	
 	public Player() {
 		
@@ -15,12 +19,35 @@ public class Player {
 	
 	public void init(){
 		camera = new OrthographicCamera(1280 * 2, 720 * 2);
+		camera.zoom -= 0.75f;
+		camera.position.set(16 * 5 / 2 * 32 / 2, 16 * 5 / 2 * 32 / 2, 0);
+		camera.update();
+	}
+	
+	private void tick() {
+		
+		if(Gdx.input.isKeyPressed(Keys.W)){
+			camera.position.y += speed;
+		}
+		if(Gdx.input.isKeyPressed(Keys.S)){
+			camera.position.y -= speed;
+		}
+		if(Gdx.input.isKeyPressed(Keys.D)){
+			camera.position.x += speed;
+		}
+		if(Gdx.input.isKeyPressed(Keys.A)){
+			camera.position.x -= speed;
+		}
+		
+		camera.update();
 	}
 	
 	public void render(SpriteBatch batch, float delta){
+		tick();
+		//CODE
 		
 	}
-	
+
 	public void dispose(){
 		
 	}
@@ -32,5 +59,14 @@ public class Player {
 	public Vector3 getWorldMouseLocation(){
 		return camera.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
 	}
+
+	
+	
+	
+	
+	
+	
+	
+
 	
 }
--- desktop/src/com/idm/game/desktop/DesktopLauncher.java
+++ desktop/src/com/idm/game/desktop/DesktopLauncher.java
@@ -5,10 +5,12 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.idm.game.Main;
 
 public class DesktopLauncher {
+	
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1280;
 		config.height = 720;
 		new LwjglApplication(new Main(), config);
 	}
+	
 }
